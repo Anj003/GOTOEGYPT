4222792905ee618c863d2fb8f014157279b8cdef
Anj003 <Anjalimathur1921@gmail.com>
2020-11-16 12:02:19 +0530

    first commit

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const header = document.querySelector('.header.container');
+hamburger.addEventListener('click',()=>{
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll',()=>{
+    var scroll_position = window.scrollY;
+    if(scroll_position > 250){
+        header.style.backgrounColor ="#5e5e71";
+    }else{
+        header.style.backgrounColor ="#transparent";
+    }
+});
